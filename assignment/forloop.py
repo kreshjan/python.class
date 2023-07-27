85bf56df24ab70e43284002d2f782128c962dc66
kreshjan <kreshmaahmadi789@gmail.com>
2023-07-27 03:43:51 -0700

    3 new files added( forloop,if.py,while.py)

---
+++ assignment/forloop.py
@@ -0,0 +1,14 @@
+numbers = [1,2,3,4,5,6,7,8,9,10]
+for x in numbers:
+    if x == 2:
+        print(x)
+    if x == 4:
+        print(x) 
+    if x ==6:
+        print(x)
+    if x == 8:
+        print(x)
+    if x ==10:
+        print(x)  
+    
+                     
---
+++ assignment/if.py
@@ -0,0 +1,7 @@
+ahmad = 25
+if ahmad==25:
+    print("this is correct")
+elif ahmad>25:
+    print("this is not correct")
+elif ahmad < 25:
+    print("this is not correct")        
